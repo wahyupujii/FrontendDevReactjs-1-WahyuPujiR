74f4514d1a2b8966d7f23643a8e5a7a543db8bdf
wahyupujii <wahyupr952@gmail.com>
2023-06-13 02:45:48 +0700

    add key to env file

--- src/config/axios.js
+++ src/config/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
     baseURL: "https://tripadvisor16.p.rapidapi.com/api/v1/restaurant/searchRestaurants",
     headers: {
-        'X-RapidAPI-Key': "X-RapidAPI-Key Anda",
+        'X-RapidAPI-Key': import.meta.env.VITE_RAPID_API_KEY,
         'X-RapidAPI-Host': 'tripadvisor16.p.rapidapi.com'
     },
     params: { locationId: '304554' },
